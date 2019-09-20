aed38e27d9f56b01e4ea409aa79ed013bce25f73
Nego Thales <thale.serra97@gmail.com>
2019-09-20 17:08:21 -0300

    schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+
+        if(!$this->app->isLocal()){
+            \URL::forceScheme('https');
+        }
     }
 
     /**
