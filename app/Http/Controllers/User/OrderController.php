dfdc097ed839f96223cb2b99c383704b2c643bc2
negothales97 <thales.serra97@gmail.com>
2020-05-22 15:55:52 -0300

    debug

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -173,7 +173,6 @@ class OrderController extends Controller
 
     public function callback(Request $request)
     {
-        return response()->json($request->all());
         $svCheckout = new ServiceCheckout;
         $notification = $request->notificationCode;
         $data['email'] = config('services.pagseguro.pagseguro_email');
